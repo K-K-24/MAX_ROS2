b792d765746655d6ebf69f6dd65d5bf903e875ca
K-K-24 <krishna24002@gmail.com>
2025-09-10 06:54:44 +0530

    urdf change

--- src/roomba_bringup/launch/roomba_ros2_control.launch.py
+++ src/roomba_bringup/launch/roomba_ros2_control.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
     serial_port = LaunchConfiguration("serial_port",default="/dev/rplidar")
     serial_baudrate = LaunchConfiguration('serial_baudrate', default='460800')
     frame_id = LaunchConfiguration('frame_id', default='laser')
-    inverted = LaunchConfiguration('inverted', default='true')
+    inverted = LaunchConfiguration('inverted', default='false')
     angle_compensate = LaunchConfiguration('angle_compensate', default='true')
     scan_mode = LaunchConfiguration('scan_mode', default='Standard')
     
